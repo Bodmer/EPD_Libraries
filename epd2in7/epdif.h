1a352cd583f3dde53aa607d31ff301d6fa4043b0
Bodmer <bodmer@anola.net>
2018-04-01 01:04:50 +0100

    Add ESP8266 and TFT_eSPI compatibility

--- epd2in7/epdif.h
+++ epd2in7/epdif.h
@@ -7,11 +7,11 @@
  *  Copyright (C) Waveshare     August 10 2017
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documnetation files (the "Software"), to deal
+ * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
  * copies of the Software, and to permit persons to  whom the Software is
- * furished to do so, subject to the following conditions:
+ * furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
@@ -30,11 +30,18 @@
 
 #include <arduino.h>
 
-// Pin definition
-#define RST_PIN         4
-#define DC_PIN          2
-#define CS_PIN          15
-#define BUSY_PIN        21
+// Pin definition for ESP8266 (e.g. NodeMCU)
+// Connect display CLK signal to GPIO 14 (Node MCU pin D5)
+// Connect display DIN signal to GPIO 13 (Node MCU pin D7)
+// Connect display 3.3V to NodeMCU 3V3
+// Connect display GND to NodeMCU GND
+// Connect other pins as follows:
+//      Display       GPIO   NodeMCU pin
+#define BUSY_PIN        5 // D1
+#define RST_PIN         4 // D2
+#define DC_PIN          0 // D3
+#define CS_PIN          2 // D4
+
 
 class EpdIf {
 public:
